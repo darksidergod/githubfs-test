c95f1114a8e35a89c3ac457658b151b6b084227f
Prajjwal <pujjumehta@gmail.com>
2020-07-22 12:06:14 +0530

    using as a module

---
+++ demo/main.go
@@ -0,0 +1,22 @@
+func main() {
+	githubToken := "11ddc8caef2a0a84649dcf3b6ca2db4f4b1d9b0b"
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: githubToken},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	fs, err := newGithubfs(client, "darksidergod", "githubfs-test", "master")
+	if err != nil {
+		panic(err)
+	}
+	//info, _ := afero.ReadDir(fs, "/")
+	//err = fs.Remove("/base.yaml")
+	//data, _ := afero.ReadFile(fs, "/core.yaml")
+	//os.Stdout.Write(data)
+	//err = fs.RemoveAll("/channel-artifacts")
+	err = fs.Rename("/configtx.txt", "/configtx.yaml")
+	fmt.Printf("%# v", pretty.Formatter(err))
+}
